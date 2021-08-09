e62d8f6198c7d97affa55ffe5aab2d3e8d4b86d6
nikethanrai <nikethanrai58@gmail.com>
2021-08-10 00:52:30 +0530

    app ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { useState } from 'react';
+import {Link} from 'react-scroll'
+
 
 function App() {
 
   const [sum,setSum]=useState('')
   const [luckyNum,setLuckyNum]=useState('')
   const [text,setText]=useState('')
+  const [dateCheck,setDateCheck]=useState('')
+  const [luckyNumCheck,setLuckyNumCheck]=useState('')
   let x=''
 
   function dateInputHandler(input){
@@ -23,22 +27,48 @@ function App() {
 
   }
   function checkBtnHandler(){
+
+
+    if(sum==2018){
+      setDateCheck('')
+      setLuckyNumCheck('')
+      
+      setText("💜's Birthday, obviously lucky")
+    return;
+  }
+    else if (sum==0 || luckyNum==''){{
+     
+        
+     } if(sum==0){
+        setDateCheck("Date can't be empty!!")
+        
+      }else{setDateCheck('')}
+      if(luckyNum==''){
+        setLuckyNumCheck('Add your Lucky Number!!')
+      }else{
+        setLuckyNumCheck('')
+      }
+      
+    }else {
+      setDateCheck('')
+      setLuckyNumCheck('')
     if (sum%luckyNum==0){
       x ='Your Birthday is Lucky'
      
     }else{
       x ='Your Birthday is not Lucky'
       
-    }
+    }}
     setText(x)
 
   }
+  
 
   return (
     <div className="App">
       <div className='page-heading'>
         <h1>Is your Birthday Lucky?</h1>
-        <a href='#main'>Scroll down</a>
+        <Link to='main' spy={true} smooth={true}><p id='letsStart'>Let's Start</p> </Link>
 
 
       </div>
@@ -49,7 +79,9 @@ function App() {
             <label id='birthday-label'>Enter your Birthday</label><br></br><input onChange={dateInputHandler} type='date' id='birthday'></input></form>
 
           <form>
+          <p className='checkInput'>{dateCheck}</p>
             <label id='lucky-label'>Your Lucky Number</label><br></br><input onChange={luckyNumberInputHandler} type="number" id='lucky-input' /></form>
+            <p className='checkInput'>{luckyNumCheck}</p>
             <button onClick={checkBtnHandler} type='submit' id='check-btn'>check</button> 
             <p>{text}</p> 
 
